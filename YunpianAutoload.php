6dc6e6fcfc38372aa9853a6e9e08e6d6436643ea
jessynt <jessynt@users.noreply.github.com>
2016-06-20 15:43:31 +0800

    fix bug

--- YunpianAutoload.php
+++ YunpianAutoload.php
@@ -9,7 +9,6 @@ require_once 'config.php';
 function YAutoload($classname)
 {
     $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib/'. $classname . '.php';
-    echo $filename;
     if (is_readable($filename)) {
         require $filename;
     }
